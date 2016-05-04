57b4bda5dcf0c899bb8fb54f4f54f97b51a65e09
Patrick <revmydev@gmail.com>
2016-05-04 14:24:13 -0700

    Initial commit Old program to get practice with output and decrementing loops. Added timing to make it more 'human' speed.

---
+++ bottlesOfBeer/bottles_of_beer.cpp
@@ -0,0 +1,31 @@
+// 99 bottles of beer on the wall
+#include <iostream>
+#include <conio.h>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{    
+    int i;
+       
+    for(i = 99; i > 0; i--)
+    {
+          if(i > 3)
+             cout << i << " bottles of beer on the wall, " << i << " bottles of beer,\n"
+                  << "Take one down, pass it around, " << i-1 << " bottles of beer on the wall.\n";
+          if(i == 2)
+          {
+             cout << i << " bottles of beer on the wall, " << i << " bottles of beer,\n"
+                  << "Take one down, pass it around, " << i-1 << " bottle of beer on the wall\n";
+          }
+          else if(i < 2)
+          {
+              cout << i << " bottle of beer on the wall, " << i << " bottle of beer,\n"
+                   << "Take it down, pass it around...and then we're all out of beers on the wall!\n";
+          }// end if
+		  Sleep(4000);
+    }// end for
+    
+    cout << "\nPresh any kee too continyoooo...";
+    _getch();
+}// end main()
